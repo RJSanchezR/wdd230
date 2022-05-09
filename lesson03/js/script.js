4ea9b74252a914d158148b50055df0ab89e515f6
RJSanchezR <san16071@byui.edu>
2022-05-09 02:34:22 -0600

    changes

--- lesson03/js/script.js
+++ lesson03/js/script.js
@@ -2,3 +2,12 @@ new Date().getFullYear()
 
 document.querySelector('#lastmod').textContent = document.lastModified;
 document.querySelector("#currentYear").textContent = new Date().getFullYear();
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
