e39a9452d19dcc59f3379f241328f9bf0e38bfd0
RJSanchezR <san16071@byui.edu>
2022-05-16 13:08:38 -0600

    changes

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -10,3 +10,20 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // // To solve the mid resizing issue with responsive class on
 // window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
