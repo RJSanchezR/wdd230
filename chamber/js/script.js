362ae2d10daa85e284979ea54835dcc1259f9603
RJSanchezR <san16071@byui.edu>
2022-05-23 14:29:15 -0600

    script for banner

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,29 +1,42 @@
+// Last modified and current year on footer
+
 new Date().getFullYear()
 
 document.querySelector('#lastmod').textContent = document.lastModified;
 document.querySelector("#currentYear").textContent = new Date().getFullYear();
+
+// Hamburger menu for responsive page
 
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
-// // To solve the mid resizing issue with responsive class on
-// window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
-// select the elements to manipulate (output to)
+// For the Date displayed
 const datefield = document.querySelector(".date");
 const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
 
-// derive the current date using a date object
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
+
+const fulldate = new Intl.DateTimeFormat("en-US", { 
+	dateStyle: "full" 
+}).format(now);
+
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
-// long, medium, short options ... try them
+
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+// For the Banner that shows on Mondays and Tuesdays
+
+const banner = document.querySelector(".banner");
+
+if (d.getDay() == 1 || d.getDay() == 2) {
+    banner.style.display = "block";
+}
+else {
+    banner.style.display = "none";
+}
