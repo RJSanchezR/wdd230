f20af700ed1e2bb9439d636075224961133b9c9d
RJSanchezR <san16071@byui.edu>
2022-06-06 04:16:52 -0600

    lazy load added

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,17 +1,18 @@
-// Last modified and current year on footer
+// For Last modified and current year on footer
 
 new Date().getFullYear()
 
 document.querySelector('#lastmod').textContent = document.lastModified;
 document.querySelector("#currentYear").textContent = new Date().getFullYear();
 
-// Hamburger menu for responsive page
+// For Hamburger menu for responsive page
 
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 hambutton.addEventListener('click', () => {hambutton.classList.toggle('responsive')}, false);
+
 // For the Date displayed
 
 const datefield = document.querySelector(".date");
@@ -35,4 +36,39 @@ if (d.getDay() == 1 || d.getDay() == 2) {
 else {
     banner.style.display = "none";
 	console.log('none')
+}
+
+// For Lazy Loading images
+
+const imagesToLoad = document.querySelectorAll("img[data-src]")
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {image.removeAttribute("data-src");};
+};
+
+if("IntersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach((item) => {
+            if (!item.isIntersecting) {
+                return;
+            } else {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+      });
 }
