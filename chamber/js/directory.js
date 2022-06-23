906fd470637ba38132758662b107b1d9d6fcbdb8
RJSanchezR <san16071@byui.edu>
2022-06-23 09:58:00 -0600

    changes

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -18,29 +18,32 @@ document.querySelector('div.cards').appendChild(s1);
 
 function displayBusiness(business) {
     let card = document.createElement('div');
-    let h2 = document.createElement('h2');
-    let p1 = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
+    let businessName = document.createElement('h2');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('p');
     let logo = document.createElement('img');
 
     logo.className = 'businessImg';
     card.className = 'businessData';
-    p1.className = 'card-desc';
-    h2.textContent = `${business["name"]}`;
-    p1.textContent = `Address: ${business["street"]} ${business["city"]} ${business["state"]} ${business["zipcode"]}`;
-    p2.textContent = `Phone Number: ${business["phone"]}`;
-    p3.textContent = `Website: ${business["website"]}`;
+    businessName.className = 'business-name';
+    address.className = 'business-add';
+    phone.className = 'business-phone';
+    website.className = 'business-website';
+    businessName.textContent = `${business["name"]}`;
+    address.textContent = `${business["street"]} ${business["city"]} ${business["state"]} ${business["zipcode"]}`;
+    phone.textContent = `${business["phone"]}`;
+    website.textContent = `${business["website"]}`;
 
     logo.setAttribute('src', `${business["imageurl"]}`);
     logo.setAttribute('alt', `Logo of ${business["name"]}`);
     logo.setAttribute('loading', 'lazy');
 
-    card.appendChild(h2);
+    card.appendChild(businessName);
     card.appendChild(logo);
-    card.appendChild(p1);
-    card.appendChild(p2);
-    card.appendChild(p3);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
 
 
     document.querySelector('div.cards section').appendChild(card);
