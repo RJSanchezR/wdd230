9c22391af23a524e782c1a513de4e177a65a5c4f
RJSanchezR <san16071@byui.edu>
2022-07-02 14:50:21 -0600

    details on memberships

--- chamber/js/form.js
+++ chamber/js/form.js
@@ -1,10 +1,24 @@
-const now = Date.now();
-document.querySelector(".submissionDate").value = now;
+// Responsive Membership level
 
+const np = document.querySelector('#np');
+const pricenp = document.querySelector('#pricenp');
 
-// document.querySelector('form').addEventListener('submit', (e) => {
-//     const formData = new FormData(e.target);
-//     // Now you can use formData.get('foo'), for example.
-//     // Don't forget e.preventDefault() if you want to stop normal form .submission
-// });
+np.addEventListener('click', () => {pricenp.classList.toggle('special')}, false);
 
+const bronze = document.querySelector('#bronze');
+const pricebronze = document.querySelector('#pricebronze');
+
+bronze.addEventListener('click', () => {pricebronze.classList.toggle('special')}, false);
+
+const silver = document.querySelector('#silver');
+const pricesilver = document.querySelector('#pricesilver');
+
+silver.addEventListener('click', () => {pricesilver.classList.toggle('special')}, false);
+
+const gold = document.querySelector('#gold');
+const pricegold = document.querySelector('#pricegold');
+
+gold.addEventListener('click', () => {pricegold.classList.toggle('special')}, false);
+
+const rightnow = Date.now();
+document.querySelector("#submissionDate").value = rightnow;
