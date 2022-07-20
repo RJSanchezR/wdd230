a6bdafcccb7010d5507d7f04eaa864a7ce5f0c27
RJSanchezR <san16071@byui.edu>
2022-07-20 17:21:45 -0600

    changes

--- temple-inn-suites/js/weather.js
+++ temple-inn-suites/js/weather.js
@@ -5,13 +5,14 @@ const captionDesc = document.querySelector('.weather-desc');
 const humidity = document.querySelector('.humidity');
 
 const base_url = 'https://api.openweathermap.org/data/2.5/weather';
-const city_name = 'Rexburg';
-const state_code = 'ID';
-const country_code = 'US';
+const city_name = 'Mexico City';
+// const state_code = 'ID';
+const country_code = 'MX';
 const appid = '6c4951197d9add3bbb5ef3b85302732f';
 const units = 'imperial';
 
-const url = `${base_url}?q=${city_name},${state_code},${country_code}&units=${units}&appid=${appid}`;
+// const url = "https://api.openweathermap.org/data/2.5/onecall?lat=19.432608&lon=-99.133209&units=imperial&appid=6c4951197d9add3bbb5ef3b85302732f";
+const url = `${base_url}?q=${city_name},${country_code}&units=${units}&appid=${appid}`;
 
 
 fetch(url)
@@ -24,12 +25,12 @@ fetch(url)
         
         const desc = data['weather'][0].description;
 
-        const humid = data['humidity']
+        const humid = data['main']['humidity'];
 
         currentTemp.textContent = Math.round(temperature);
         weatherIcon.setAttribute('src', iconsrc);
         weatherIcon.setAttribute('alt', desc);
         captionDesc.textContent = desc;
         humidity.textContent = humid;
-        city.textContent = `${city_name}, ${state_code}`;
+        city.textContent = `${city_name}, ${country_code}`;
     });
